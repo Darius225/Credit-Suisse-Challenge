7108cf8c47aec84271f8dbf06e86d84e583abe67
darius <darius.grigore.pop10@gmail.com>
2018-10-15 18:33:30 +0100

    Solution to first problem

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -3,9 +3,18 @@ package answers;
 public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
-		// TODO Auto-generated method stub
-
-		return -1;
+		int n = portfolios.length ;
+		int maxValue = 0 ;
+		for (int i = 0; i < n - 1 ; i++) {
+			for (int j = i + 1 ; j < n ; j++) {
+				int pairValue = portfolios [ i ] ^ portfolios [ j ] ;
+				if ( pairValue > maxValue )
+				{
+					maxValue = pairValue ;
+				}
+			}
+		}
+		return maxValue;
 	}
 
 }
