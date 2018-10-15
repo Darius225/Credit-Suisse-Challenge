de25d3ce3903e3363bae299582e2bc3d4fec56d3
darius <darius.grigore.pop10@gmail.com>
2018-10-15 20:24:35 +0100

    Solution to work with long values

--- src/main/java/answers/Question6.java
+++ src/main/java/answers/Question6.java
@@ -6,20 +6,20 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Question6 {
-    private static final int INF = Integer.MAX_VALUE ;
-    public static int shortestServerRoute(int numServers, int targetServer, int[][] times) {
+    private static final long INF = Long.MAX_VALUE ;
+    public static long shortestServerRoute(int numServers, int targetServer, int[][] times) {
         Map graph = new Map();
         graph.setArcs(times);
         graph.setNumServers(numServers);
         graph.setTarget(targetServer);
         return bellmanFord(graph);
     }
-    private static int bellmanFord(Map graph)
+    private static long bellmanFord(Map graph)
     {
         Queue<Integer> servers = new LinkedList<>();
         int n = graph.getNumServers();
         int k = graph.getTarget();
-        int[] bestDistances = new int[n];
+        long[] bestDistances = new long[n];
         initializeWithINFValue(bestDistances);
         int[][] costs = graph.getArcs();
         bestDistances [ 0 ] = 0 ;
@@ -27,23 +27,20 @@ public class Question6 {
         while( ! servers.isEmpty() )
         {
             int candidateServer = ((LinkedList<Integer>) servers).pollFirst();
-            int distanceFromSource = bestDistances [ candidateServer ] ;
+            long distanceFromSource = bestDistances [ candidateServer ] ;
             for (int dest = 0; dest < n ; dest ++) {
-                if ( candidateServer != dest )
-                {
                     int cost = costs [ candidateServer ] [ dest ] ;
-                    int bestDistanceToDest = bestDistances [ dest ] ;
+                    long bestDistanceToDest = bestDistances [ dest ] ;
                     if ( bestDistanceToDest > distanceFromSource + cost )
                     {
                         bestDistances [ dest ] = distanceFromSource + cost ;
                         servers.add(dest);
                     }
-                }
             }
         }
         return bestDistances[k] ;
     }
-    private static void initializeWithINFValue ( int[] array )
+    private static void initializeWithINFValue ( long[] array )
     {
         int n = array.length ;
         for (int i = 0; i < n ; i++)
