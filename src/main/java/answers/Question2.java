04edf73c1b90dcba0fcb043064b33af800be95ae
darius <darius.grigore.pop10@gmail.com>
2018-10-17 11:18:19 +0100

    Attempted second problem

--- src/main/java/answers/Question2.java
+++ src/main/java/answers/Question2.java
@@ -1,10 +1,56 @@
 package answers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Question2 {
 
 	public static int equallyBalancedCashFlow(int[] cashflowIn, int[] cashflowOut) {
-		// TODO Auto-generated method stub
-		return -1;
+
+		//Sort the arrays as we intend to use a greedy approach
+		Arrays.sort(cashflowIn);
+		Arrays.sort(cashflowOut);
+
+		//We can choose one subset to be empty
+		int ans = Math.min( cashflowIn [ 0 ] , cashflowOut [ 0 ] ) ;
+
+		int n = cashflowIn.length;
+		int m = cashflowOut.length;
+
+		int inSum = cashflowIn [ 0 ] ;
+		int outSum = cashflowOut [ 0 ] ;
+
+		for (int i = 1 , j = 1 ; i < n && j < m ; )
+		{
+			if ( inSum > outSum )
+			{
+				if ( inSum > cashflowIn [ i ] && cashflowIn [ i ] > outSum )
+				{
+					inSum = cashflowIn [ i ] ;
+					i++;
+				}
+				else
+				{
+					outSum += cashflowOut [ j ];
+					j ++ ;
+				}
+			}
+			else if ( outSum > inSum )
+			{
+				if ( outSum > cashflowOut [ j ]  && cashflowOut [ j ] > inSum )
+				{
+					outSum = cashflowOut [ j ] ;
+					j ++ ;
+				}
+				else
+				{
+					inSum += cashflowIn [ i ] ;
+					i ++ ;
+				}
+			}
+			ans = Math.min(ans,Math.abs(inSum - outSum ) ) ;
+		}
+		return ans;
 	}
 
 }
