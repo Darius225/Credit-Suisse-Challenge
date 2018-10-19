df7045b788eed6712aa23a76b2ee2dad0b310f29
darius <darius.grigore.pop10@gmail.com>
2018-10-19 12:07:25 +0100

    Trying to improve the score on 4th question

--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -2,34 +2,41 @@ package answers;
 
 public class Question4 {
 
+	//Uglier code but hope this will improve performance
 	public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
-		int[][] intRepresentation = transformStringsToInts(rows);
+
 		int n = rows.length ;
 		int answer = Integer.MAX_VALUE ;
 		for (int i = 0; i < n ; i++) {
-			int minRow = minSumRow(intRepresentation[i],numberMachines) ;
-			if ( minRow < answer )
+			int currentSum = 0 ;
+			int currentConsecutiveValidValues = 0 ;
+			for (int j = 0; j < n ; j++)
 			{
-				answer = minRow ;
+				int value = getNumericValue( rows [ i ] [ j ] ) ;
+				if ( value >= 0 )
+				{
+					currentConsecutiveValidValues++;
+					currentSum += value ;
+					if ( currentConsecutiveValidValues >= numberMachines )
+					{
+						if ( currentSum < answer )
+						{
+							answer = currentSum ;
+						}
+						int value1 = getNumericValue( rows [ i ] [ j - numberMachines + 1 ] ) ;
+						currentSum -= value1 ;
+					}
+				}
+				else
+				{
+					currentConsecutiveValidValues = 0 ;
+					currentSum = 0 ;
+				}
 			}
 		}
 		if ( answer == Integer.MAX_VALUE )
 		{
 			return 0 ;
-		}
-		return answer ;
-	}
-	private static int[][] transformStringsToInts( String[][] rows )
-	{
-		int noRows = rows.length;
-		int noColumns = rows[0].length;
-		int[][] answer = new int[noRows][noColumns] ;
-		for (int i = 0; i < noRows ; i++)
-		{
-			for (int j = 0; j < noColumns; j++) {
-				int value = getNumericValue(rows[i][j]);
-				answer [ i ] [ j ] = value ;
-			}
 		}
 		return answer ;
 	}
@@ -40,38 +47,5 @@ public class Question4 {
 			return -1 ;
 		}
 		return Integer.parseInt(s);
-	}
-	private static int minSumRow(int[] row , int target )
-	{
-		int n = row.length ;
-		int minSum = Integer.MAX_VALUE ;
-
-		//Use just a variable to contain the sum of a valid subsequence
-		int currentSum = 0 ;
-
-		int currentConsecutiveValidNumbers = 0 ;
-		for ( int i = 0 ; i < n ; i ++ )
-		{
-			if ( row [ i ] >= 0 )
-			{
-				currentSum += row [ i ] ;
-				currentConsecutiveValidNumbers ++ ;
-				if ( currentConsecutiveValidNumbers >= target )
-				{
-					if ( currentSum < minSum )
-					{
-						minSum = currentSum ;
-					}
-					currentSum -= row [ i - target + 1 ] ;
-				}
-			}
-			else
-			{
-				//reset the variables in order to not add an invalid value to the sums
-				currentSum = 0 ;
-				currentConsecutiveValidNumbers = 0 ;
-			}
-		}
-		return minSum ;
 	}
 }
